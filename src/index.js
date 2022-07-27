b9d5ce56a50e12257134ce4908622b066c877560
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-27 14:21:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,3 +16,13 @@ export default helloTemplate;
 
 
 // https://dev.to/azure/too-hard-too-soft-just-right-rendering-html-with-lit-html-1km8
+
+
+// Component image start
+// const getImage = (id, href, src) => {
+//   return `<a href="${href}" data-link-id="${id}" target="_self">
+//   <img src="${createPathToImage(src)}"
+//   border="0" alt="" width="267" style="display: block;">
+// </a>`;
+// };
+// Component image end
