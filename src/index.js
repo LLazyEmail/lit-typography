8a6df7e0846d010315bd54fbde76f209cff8a33f
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2022-07-27 10:51:59 +0000

    github actions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 //---
+
+import { html, render } from 'lit-html';
+
+// // This is a lit-html template function. It returns a lit-html template.
+const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
+
+
+export default helloTemplate;
+
+// // This renders <div>Hello Steve!</div> to the document body
+// render(helloTemplate('Steve'), document.body);
+
+// // This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
+// render(helloTemplate('Kevin'), document.body);
+
+
+// https://dev.to/azure/too-hard-too-soft-just-right-rendering-html-with-lit-html-1km8
